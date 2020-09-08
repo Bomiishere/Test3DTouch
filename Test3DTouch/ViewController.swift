dbb2fc7aa793b24aaaf0915d665675b95b0b65fa
Bomi <bomiishere@gmail.com>
2020-09-08 10:44:38 +0800

    Initial Commit

---
+++ Test3DTouch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test3DTouch
+//
+//  Created by bomi.chen on 2020/9/8.
+//  Copyright © 2020 JohnsonTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
